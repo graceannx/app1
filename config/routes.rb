04fc77e2ff60657e5af3837575c71dd0eb988161
graceannx <graceannx@icloud.com>
2015-07-29 15:05:36 +0100

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   resources :dashboard
   resources :communities
   resources :profile
-  resources :home
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
