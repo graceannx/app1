9bc90c5aa0f083258128bd2518867ba946d0dca7
OwenHayes <owengeorghayes@googlemail.com>
2015-07-29 11:39:47 +0100

    Login pages- views styled - 11:40 29th

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :dashboard
   resources :communities
   resources :profile
+  resources :home
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
